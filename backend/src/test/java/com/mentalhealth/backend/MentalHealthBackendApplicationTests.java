3aaa84789bd545cfac7a4458aa75f1a81b1abb33
Mahmuda Rahaman <rahaman2207032@stud.kuet.ac.bd>
2025-12-09 09:06:18 +0600

    fixed backend

---
+++ backend/src/test/java/com/mentalhealth/backend/MentalHealthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentalhealth.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalHealthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
